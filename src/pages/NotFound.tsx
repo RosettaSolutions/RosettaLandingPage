0057fbefd3228c45b53a0a0043ab44ee406a142a
Kaio Vinícios <kvsgpro@outlook.com>
2025-10-29 09:28:57 -0300

    feat(pages): create NotFound.tsx

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,32 @@
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
+const NotFound = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    console.error(
+      "404 Error: User attempted to access non-existent route:",
+      location.pathname
+    );
+  }, [location.pathname]);
+
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="text-center">
+        <h1 className="mb-4 text-8xl font-extrabold italic animate-pulse">
+          404
+        </h1>
+        <p className="mb-4 text-xl text-gray-600">Oops! Page not found</p>
+        <a
+          href="/"
+          className="mt-2 text-blue-500 underline hover:text-blue-700"
+        >
+          Return to Home
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
